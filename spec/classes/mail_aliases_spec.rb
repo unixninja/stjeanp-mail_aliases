989527e978fe7d9bb156594572c4a8891910effa
Patrick St. Jean <stjeanp@pat-st-jean.com>
2014-10-24 11:50:27 -0500

    Updated rspec tests
    Updated rspec tests to test all supported $::osfamily values and
    changed the email addresses in test.yaml to dummy values.

--- spec/classes/mail_aliases_spec.rb
+++ spec/classes/mail_aliases_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 
 describe 'mail_aliases', :type => :class do
   context 'Unsupported OS' do
-    let(:facts) { {:osfamily => 'Debian'} }
+    let(:facts) { {:osfamily => 'Solaris'} }
 
     it do
       expect {
         should compile
-      }.to raise_error(Puppet::Error, /OS family 'Debian' is not supported by this module/)
+      }.to raise_error(Puppet::Error, /OS family 'Solaris' is not supported by this module/)
     end
   end
 
@@ -18,22 +18,76 @@ describe 'mail_aliases', :type => :class do
     it { should compile }
   end
 
+  context 'Debian OS and no hiera data' do
+    let(:facts) { {:osfamily => 'Debian'} }
+
+    it { should compile }
+  end
+
+  context 'Suse OS and no hiera data' do
+    let(:facts) { {:osfamily => 'Suse'} }
+
+    it { should compile }
+  end
+
   context 'RedHat OS and hiera data' do
     let(:facts) { {:osfamily => 'RedHat'} }
 
     it { should compile }
     it { should contain_mailalias('root').with(
-      'recipient' => 'stjeanp@pat-st-jean.com',
+      'recipient' => 'admin@mailbox.com',
       'notify'    => 'Exec[newaliases]',
       'ensure'    => 'present',
     ) }
     it { should contain_mailalias('puppet').with(
-      'recipient' => 'stjeanp@pat-st-jean.com',
+      'recipient' => 'puppetmaster@mailbox.com',
       'notify'    => 'Exec[newaliases]',
       'ensure'    => 'present',
     ) }
     it { should contain_mailalias('olduser').with(
-      'recipient' => 'stjeanp@pat-st-jean.com',
+      'recipient' => 'movedto@another.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'absent',
+    ) }
+  end
+
+  context 'Debian OS and hiera data' do
+    let(:facts) { {:osfamily => 'Debian'} }
+
+    it { should compile }
+    it { should contain_mailalias('root').with(
+      'recipient' => 'admin@mailbox.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'present',
+    ) }
+    it { should contain_mailalias('puppet').with(
+      'recipient' => 'puppetmaster@mailbox.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'present',
+    ) }
+    it { should contain_mailalias('olduser').with(
+      'recipient' => 'movedto@another.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'absent',
+    ) }
+  end
+
+  context 'Suse OS and hiera data' do
+    let(:facts) { {:osfamily => 'Suse'} }
+
+    it { should compile }
+    it { should contain_mailalias('root').with(
+      'recipient' => 'admin@mailbox.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'present',
+    ) }
+    it { should contain_mailalias('puppet').with(
+      'recipient' => 'puppetmaster@mailbox.com',
+      'notify'    => 'Exec[newaliases]',
+      'ensure'    => 'present',
+    ) }
+    it { should contain_mailalias('olduser').with(
+      'recipient' => 'movedto@another.com',
       'notify'    => 'Exec[newaliases]',
       'ensure'    => 'absent',
     ) }
